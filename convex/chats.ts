3ae0732287f2178ca7d9b1d46db3a02f178f5e8f
Ashutosh Swain <79051730+AshainX@users.noreply.github.com>
2025-02-04 14:15:28 +0530

    chats row

--- convex/chats.ts
+++ convex/chats.ts
@@ -1,6 +1,6 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
-
+import { chats } from "./_generated/dataModel";
 
 export const createChat = mutation({
   args: {
